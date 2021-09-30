d8c7055ccb662f46e4f3ec5126953c6e10c14c05
AndreiaLima19 <89053834+AndreiaLima19@users.noreply.github.com>
2021-09-29 23:06:31 -0300

    Aula8

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 const express = require("express");  //importando o express
-const path = require("path");  //importando o path
-const app = express();
-const port = 3000;
+const path = require("path");  // importando o path
+const app = express();  // definindo meu app como um express
+const port = 3000;  //set porta na 3000
 
-app.set("view engine", "ejs");  //set engine p trabalhar com o EJS
+app.set("view engine", "ejs");  //set engine para trabalhar com o EJS
 
-app.use(express.static(path.join(__dirname, "public")));  //set publi como pasta raiz de arquivos
+app.use(express.static(path.join(__dirname, "public")));  //set public como pasta raiz de arquivos estaticos como imgs e css
+app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
-    res.render("index", {titulo: "Olá Bluemers!!"}); // Nome do arquivo, o EJS já busca dentro da pasta views.
+app.get("/", function (req, res){ // .get para tratar as requisicoes tipo get e a rota solicitada
+    res.render("index", { titulo: "ola bluemer 123" });  // render para renderizar o arquivo EJS dentro da views por padrao
 });
 
-app.get("/pagina", (req, res) => {
-    res.render("pagina",{titulo: "Página 2!!"});
+app.get("/pagina", function (req, res){
+    res.render("pagina", { titulo: "pagina 2 123" });
 });
 
-app.get("/", function (req, res) {
-    res.send("Hello bluemers");
+app.get("/teste", function (req, res){
+    res.render("../views/ejs/pagina2", { titulo: "pagina modificada" })
 });
 
-app.get("/teste2", function (req, res) {
-    res.send("esse é um teste2");
+app.get("/formulario", function (req, res){
+    res.render("formulario");
+});
+
+app.get("/recebeform", function (req, res){  //só p mostrar o get
+    const { nome, email, senha } = req.query;
+    res.send({ nome: nome, email: email, senha: senha});
+});
+
+// app.post("/recebeform", function (req, res){
+//     const { nome, email, senha } = req.body;
+//     res.send({ nome: nome, email: email, senha: senha});
+// });
+
+app.post("/recebeform", function (req, res){
+    const { nome, email, senha, turma } = req.body;
+    const site = {titulo: "Formulário 2 - Resultado",nome: nome, email: email, senha: senha,turma:turma};
+    res.render("formulario2",site);
 });
 
 // app.post
 // app.put
 // app.delete
-
-app.listen(port,() => console.log(`Servidor rodando em http://localhost:${port}`));
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`)); // faz o servidor escutar, passando a porta onde o servico vai ficar ativo
